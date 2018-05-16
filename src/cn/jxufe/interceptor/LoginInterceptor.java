fcbde509b850e4947491743313cea954f9cef998
qiujianqiang <1457216946@qq.com>
2018-05-16 22:55:39 +0800

    用户添加拦截器

--- src/cn/jxufe/interceptor/LoginInterceptor.java
+++ src/cn/jxufe/interceptor/LoginInterceptor.java
@@ -2,14 +2,14 @@ package cn.jxufe.interceptor;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
-import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
 
 import cn.jxufe.domain.User;
 
-public class LoginInterceptor extends AbstractInterceptor {
+public class LoginInterceptor extends MethodFilterInterceptor {
 	private static final long serialVersionUID = 1L;
 	@Override
-	public String intercept(ActionInvocation invocation) throws Exception {
+	protected String doIntercept(ActionInvocation invocation) throws Exception {
 		System.out.println("µÇÂ½À¹½ØÆ÷");
 		User curUser = (User) ActionContext.getContext().getSession().get("curUser");
 		if(curUser != null) {
